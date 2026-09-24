7034facf10345abbce4bc036810477a59ae6eaab
Raushan2612 <144277478+Raushan2612@users.noreply.github.com>
2026-09-24 21:14:55 +0530

    Removing unnecessary comments

--- dynamic form/dynamic-form/index.js
+++ dynamic form/dynamic-form/index.js
@@ -1,6 +1,6 @@
-const labelInputField = document.querySelector('#labelValue')//jo label wala yani dusare column ko 
-const form = document.querySelector('#form')//ye jo naya content page par denge 
-const inputType = document.querySelector('#inputType')//ye input type ko 
+const labelInputField = document.querySelector('#labelValue')
+const form = document.querySelector('#form')
+const inputType = document.querySelector('#inputType')
 
 labelInputField.addEventListener('keyup', (event) => {
     const keyPressed = event.key
@@ -9,26 +9,25 @@ labelInputField.addEventListener('keyup', (event) => {
 
     if (keyPressed === "Enter") {
         // create inputs 
-        const type = inputType.value //jo select kiya he (options me se)
+        const type = inputType.value 
 
-        const label = labelInputField.value//jo bhi user ne type kiya he wo ye he
+        const label = labelInputField.value
 
-        const labelInput = document.createElement("label")//this create an additional html label
-//jo bhi usane label me dala he use me form isert karte samay label bana dunga
-        const div = document.createElement("div")// isliye banaya he taki div ke andar label or input type ko select kiya usko dal denge
+        const labelInput = document.createElement("label")
 
+        const div = document.createElement("div")
         let input = null;
-//suru me input ko null declare kiya he 
+
         if (type === 'textarea')
             input = document.createElement("textarea")
-            // agar input textarea hua to ek sizeable area ayega 
+           
         else
             input = document.createElement('input')
-//nhi to as usual jo input select kiya he wese aayega 
+
         labelInput.innerHTML = label
-        // jo label me type karunga 
+        
         input.type = type
-        //ye jo user type select kiya he us type ka text magenga jese sgar date select kiya date ka input format aa jayega
+       
         input.classList.add('form-control')
         labelInput.classList.add("form-label")
         div.classList.add("mb-3")
